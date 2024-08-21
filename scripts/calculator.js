07fc7dd6ad708390c3886858a01b1a69c4050d05
Rizuan <john.lee.coco@gmail.com>
2024-08-21 23:40:30 +0800

    feat: sum 2 numbers

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,7 +1,22 @@
 class Calculator {
 
     add(num1, num2) {
+        if (arguments.length > 2) {
+            throw new Error("Only 2 parameters allowed.");
+        } else if (arguments.length < 2) {
+            throw new Error("2 parameters must be provided.");
+        }
         
+        if (
+            typeof num1 !== "number" ||
+            typeof num2 !== "number" ||
+            Number.isNaN(num1) ||
+            Number.isNaN(num2))
+        {
+            throw new Error("Parameters must either be an integer or floating point number.");
+        }
+
+        return num1 + num2;
     }
 
     subtract(num1, num2) {
